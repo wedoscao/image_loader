5e7b1ac99b8aa9ad0c1a27732fa6f8f94bd6649d
Sang Cao <hoaisang0806@gmail.com>
2022-12-17 16:52:01 +0700

    first commit

--- next.config.js
+++ next.config.js
@@ -7,6 +7,10 @@ const nextConfig = {
                 protocol: "http",
                 hostname: "localhost",
             },
+            {
+                protocol: "https",
+                hostname: "image-loader-my036auow-wedoscao.vercel.app",
+            },
         ],
     },
 };
