0b7f59876fb38ddfcd6dd9756f32a1e9ee35cd69
Sang Cao <hoaisang0806@gmail.com>
2022-12-18 13:14:18 +0700

    fixed icon

--- components/HeadLayout.tsx
+++ components/HeadLayout.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent, PropsWithChildren, useEffect, useState } from "react";
+import { FunctionComponent, PropsWithChildren } from "react";
 
 import Head from "next/head";
 
@@ -6,12 +6,6 @@ interface Props {
     title?: string;
 }
 const HeadLayout: FunctionComponent<PropsWithChildren<Props>> = ({ children, title }) => {
-    const [host, setHost] = useState("");
-
-    useEffect(() => {
-        setHost(`${window.location.protocol}//${window.location.host}${window.location.pathname}`);
-    }, []);
-
     return (
         <>
             <Head>
@@ -20,14 +14,10 @@ const HeadLayout: FunctionComponent<PropsWithChildren<Props>> = ({ children, tit
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <meta name="msapplication-TileColor" content="#da532c" />
                 <meta name="theme-color" content="#ffffff" />
-                {host ? (
-                    <>
-                        <link rel="apple-touch-icon" sizes="180x180" href={`${host}/icons/apple-touch-icon.png`} />
-                        <link rel="icon" type="image/png" sizes="32x32" href={`${host}/icons/favicon-32x32.png`} />
-                        <link rel="manifest" href={`${host}/icons/site.webmanifest`} />
-                        <link rel="mask-icon" href={`${host}/icons/safari-pinned-tab.svg`} color="#5bbad5" />
-                    </>
-                ) : null}
+                <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+                <link rel="manifest" href="/icons/site.webmanifest" />
+                <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5" />
             </Head>
             {children ? children : null}
         </>
