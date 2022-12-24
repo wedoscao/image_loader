10e391e17555b21e6b48a3dd0838a1a6c4de31af
Sang Cao <hoaisang0806@gmail.com>
2022-12-24 17:31:09 +0700

    fixed image

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,48 +8,31 @@ import fs from "fs";
 
 interface Image {
     id: number;
-    name: string;
+    image: string;
 }
 
 interface Props {
     data: Image[];
 }
 const Home = (props: Props) => {
-    const [host, setHost] = useState("");
     const router = useRouter();
-
-    useEffect(() => {
-        if (window !== undefined) {
-            setHost(`${window.location.protocol}//${window.location.host}${window.location.pathname}`);
-        }
-    }, []);
-
-    const handledClickImage = (event: React.MouseEvent<HTMLDivElement, MouseEvent>, pathname: string) => {
-        router.push(pathname);
-    };
 
     return (
         <HeadLayout>
             <MainLayout>
                 <div className="mt-16 grid h-screen w-full grid-cols-6">
-                    {host
-                        ? props.data.map((image) => {
-                              return (
-                                  <div
-                                      key={image.id}
-                                      className="xl:col-span-1 lg:col-span-2 sm:col-span-3 col-span-6 h-80 relative hover:opacity-90 cursor-pointer"
-                                      onClick={(e) => handledClickImage(e, `/images/${image.name}`)}
-                                  >
-                                      <Image
-                                          alt={`${image.name}`}
-                                          src={`${host}images/${image.name}`}
-                                          fill
-                                          loading="lazy"
-                                      />
-                                  </div>
-                              );
-                          })
-                        : null}
+                    {props.data.map((image) => {
+                        return (
+                            <div
+                                key={image.id}
+                                className="xl:col-span-1 lg:col-span-2 sm:col-span-3 col-span-6 h-80 relative hover:opacity-90 cursor-pointer text-white"
+                            >
+                                <a href={image.image}>
+                                    <Image alt={`ERROR`} src={`${image.image}`} fill loading="lazy"></Image>
+                                </a>
+                            </div>
+                        );
+                    })}
                 </div>
             </MainLayout>
         </HeadLayout>
@@ -58,14 +41,8 @@ const Home = (props: Props) => {
 export default Home;
 
 export const getStaticProps = async () => {
-    const fileDir = path.join(process.cwd(), "public", "images");
-    const paths = fs.readdirSync(fileDir);
-    const data: Image[] = paths.map((path, index): Image => {
-        return {
-            id: index,
-            name: path,
-        };
-    });
+    const publicPath = path.join(process.cwd(), "public");
+    const data: Image[] = JSON.parse(fs.readFileSync(path.join(publicPath, "image.json"), "utf-8"))["images"];
     return {
         props: { data },
     };
